e65afb0792d6be3169ef138d14477095f14b2a63
Giovanni Uchoa <giovanni.uchoa@camposdealer.com>
2026-02-23 16:22:00 -0300

    Adicionar arquivos de projeto.

---
+++ MongoDB/Controllers/CustomerController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Data;
+using MongoDB.Data.Entities;
+using MongoDB.Driver;
+
+namespace MongoDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+
+        private readonly IMongoCollection<Customer> _customers;
+        
+        public CustomerController(MongoDbService mongoDbService) 
+        {
+            _customers = mongoDbService.Database?.GetCollection<Customer>("customer");
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Customer>> Get()
+        {
+            return await _customers.Find(FilterDefinition<Customer>.Empty).ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer?>> GetById(string id)
+        {
+
+            var filter = Builders<Customer>.Filter.Eq(x => x.Id, id);
+            var customer = _customers.Find(filter).FirstOrDefault();
+
+            return customer is not null ? Ok(customer) : NotFound();
+
+        }
+
+    }
+}
---
+++ MongoDB/Data/Entities/Customer.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDB.Data.Entities
+{
+    public class Customer
+    {
+        [BsonId]
+        [BsonElement("_id"), BsonRepresentation(Bson.BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("customer_name"), BsonRepresentation(Bson.BsonType.String)]
+        public string? CustomerName { get; set; }
+
+        [BsonElement("email"), BsonRepresentation(Bson.BsonType.String)]
+        public string? Email { get; set; }
+    }
+}
